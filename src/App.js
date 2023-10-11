9e1de00e57b42b87577b07c517fc1875a7356762
Radharani785252 <Radharani785252@gmail.com>
2023-10-11 10:33:03 +0530

    fronted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
 function App() {
+  const [to, setTo] = useState("");
+  const [body, setBody] = useState("");
+  const [response, setResponse] = useState("");
+
+  const sendSMS = async () => {
+    try {
+      const response = await fetch("https://message-y4ck.onrender.com/sendsms",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ to, body }),
+        }
+      );
+
+      const data = await response.json();
+      setResponse(data.message);
+    } catch (error) {
+      console.error(error);
+      setResponse("Failed to send SMS");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Send SMS</h1>
+      <div>
+        <label>To:</label>
+        <input type="text" value={to} onChange={(e) => setTo(e.target.value)} />
+      </div>
+      <div>
+        <label>Message:</label>
+        <input
+          type="text"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+      <button onClick={sendSMS}>Send SMS</button>
+      <div>
+        <p>{response}</p>
+      </div>
     </div>
   );
 }
